b91d4bbd28ce554415c2db5df6b77d1665872632
sphelelentando <thbkmgijima@gmail.com>
2026-08-08 12:54:00 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+window.LITHA_CONFIG = {
+  brand: 'L.I.T.H.A.',
+  company: 'Litha Brands (Pty) Ltd',
+  supportEmail: 'official.litha@gmail.com',
+  currency: 'ZAR',
+  freeShippingThreshold: 1500,
+  standardShippingEstimate: 120,
+  instagram: '',
+  tiktok: '',
+  analyticsMeasurementId: '',
+  paymentMode: 'order-request' // Keep until secure server-side payment integration is connected.
+};
